d4ca9463d27fb8ac141d6afb9fac93bc133ac3fd
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-15 16:17:01 -0300

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,5 @@
+let nota1 = Number(prompt("inisra a 1ª nota do aluno: "));
+let nota2 = Number(prompt("insira a 2ª nota do aluno: "));
+let nota3 = Number(prompt("insira a 3ª nota do aluno: "));
+const total = (nota1 + nota2 + nota3) / 3;
+alert("a media das notas é de: " + total);
