6050a3d23191feec4be5dbb90f523e3f40c25dbe
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-15 16:41:59 -0300

    Create 10.js

---
+++ 10.js
@@ -0,0 +1,9 @@
+let distancia = Number(prompt("inisra a distamcia em km: "))
+let preço = Number(prompt("insira o preço da gasolina: "))
+let litro = Number(prompt("insira o comsumo por km: "))
+const total = (distancia / litro) * preço
+alert("o total a ser gasto é de: R$" + total);
+
+
+
+
