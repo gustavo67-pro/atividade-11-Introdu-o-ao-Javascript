1ff1de090b6103dc48eafa7ad868fa6b1026dd30
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-15 16:24:17 -0300

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,9 @@
+let salario = Number(prompt("inisra o salario atual do funcionario: "))
+let aumento = (salario * 20) / 100
+const salarioreal = salario + aumento
+alert("o salario atual agora é de: " + salarioreal)
+
+
+
+
+
