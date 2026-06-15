b6840c4cc99c4615b5d4587dab7780a007217d10
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-15 16:10:24 -0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,9 @@
+let conta = Number(prompt("insira o total da conta: "));
+let amigos = Number(prompt("insira a quantidade de pessoas"));
+const total = conta / amigos
+alert("a divisao ficou: " + total)
+
+
+
+
+
